c1fb8266f948ef01f83a16631427b7f6e52b7893
Gabriel Brown <gabriel.brown@rallyhealth.com>
2020-01-17 17:48:13 -0600

    add route for processing stats

--- server.py
+++ server.py
@@ -1,12 +1,43 @@
-from flask import Flask, make_response, request, jsonify
+from flask import Flask, render_template, make_response, request, jsonify
 from flask_socketio import SocketIO, emit
+import json
 
 app = Flask(__name__)
 socketio = SocketIO(app, ping_interval=1, ping_timeout=3, transports=['websocket']) 
 
+players = {}
+game_stats = {}
+
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+@app.route('/stats', methods=["POST"])
+def stats():
+    """
+    Handles incoming stats and events from kqstats 
+    (the service that connects directly to the cab)
+    """
+    # TODO: Do some processing to connect player names to game characters
+    # TODO: Send processed data to firebase to store
+
+    # Using XMLHttpRequest on kqstats isn't sending the content type
+    # correctly for some reason, which is why we need to to add this 
+    # force=True. Should be able to do this in a less wonky way once kqstats
+    # is updated to use ajax or something like that.
+    stats = request.get_json(force=True)
+    if stats == None:
+        stats = {}
+
+    # For now, just echo back what was received
+    print("Stats received: ")
+    print(stats)
+
+    print("Request received:")
+    print(request)
+
+    return make_response(stats)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
