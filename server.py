eea56bac26d9d35a01be5948c4c198fa8faa824b
Gabriel Brown <gabriel.brown@rallyhealth.com>
2020-01-17 21:20:36 -0600

    add route for displaying stats

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 socketio = SocketIO(app, ping_interval=1, ping_timeout=3, transports=['websocket']) 
 
 players = {}
-game_stats = {}
+game_stats = []
 
 @app.route('/')
 def index():
@@ -26,8 +26,11 @@ def stats():
     # force=True. Should be able to do this in a less wonky way once kqstats
     # is updated to use ajax or something like that.
     stats = request.get_json(force=True)
-    if stats == None:
+    if stats != None:
+        game_stats.append(stats)
+    else:
         stats = {}
+        print("Empty stats set received")
 
     # For now, just echo back what was received
     print("Stats received: ")
@@ -36,7 +39,14 @@ def stats():
     print("Request received:")
     print(request)
 
+    game_stats.append(stats)
+
     return make_response(stats)
+
+@app.route('/viewstats', methods=['GET'])
+def view_stats():
+
+    return render_template("view_stats.html", game_stats=game_stats)    
 
 
 if __name__ == '__main__':
